6a4b9fa446fc596066aef55cf23985cca378a523
Anshul Kumar <f20230621@goa.bits-pilani.ac.in>
2025-12-07 22:14:08 +0530

    asd

--- api/app.py
+++ api/app.py
@@ -1731,7 +1731,7 @@ def get_corporate_actions(current_user):
     
 
 @app.route('/api/corporate_filings', methods=['GET', 'OPTIONS'])
-# @auth_required
+@auth_required
 def get_corporate_filings():
     """Endpoint to get corporate filings with server-side pagination"""
     if request.method == 'OPTIONS':
